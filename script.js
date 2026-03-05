6799d104662ba2a4decefb49edd5642a02dfd980
THANWEER <tanveerkhanktm@gmail.com>
2026-03-05 20:37:59 +0530

    Fix formatting issues in getWeather function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function getWeather(){
 
 let city=document.getElementById("cityInput").value;
 
-let apiKey=95e7277869396392f75c4bad32ec1ff2;
+let apiKey="95e7277869396392f75c4bad32ec1ff2";
 
 let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
@@ -15,5 +15,6 @@ document.getElementById("temp").innerText="Temperature: "+data.main.temp+" °C";
 document.getElementById("desc").innerText="Condition: "+data.weather[0].description;
 document.getElementById("humidity").innerText="Humidity: "+data.main.humidity+" %";
 document.getElementById("wind").innerText="Wind Speed: "+data.wind.speed+" m/s";
+
 
 }
